2e4bad3447fc45119a985a3b9ddfe3ad8e08a6e9
yaelitovi <36213360589@mby.co.il>
2023-01-18 19:24:06 +0200

    mergeAngular

---
+++ ny-tasks/src/models/users.model.ts
@@ -0,0 +1,13 @@
+
+export class userModel{
+  iPersonId :number=0;
+  nvUserName :String ="";
+  nvPasswordperty :String ="";
+  nvPasswordHint :String ="";
+  iLastUpdateUserId:number=0;
+  dtLastUpdateDate:Date=new Date();
+  iSysRowStatus :number=0;
+  iPersonStatusId :number=0;
+  iAdvancedOptionsId :number=0;
+  iValidUserNameStatus :number=0;
+}
