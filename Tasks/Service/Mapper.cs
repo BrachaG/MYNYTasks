5b92e2d67f996b3a4d697b86735a6d7c6567cac0
ayalatamar <36214121071@mby.co.il>
2023-02-08 15:09:11 +0200

    perfect

--- Tasks/Service/Mapper.cs
+++ Tasks/Service/Mapper.cs
@@ -28,31 +28,7 @@ namespace Service
             CreateMap<DataRow, User>()
              .ConvertUsing(row => MapDataRowToEntity<User>(row));
             CreateMap<DataRow, CodeTable>()
-             .ConvertUsing(row => MapDataRowToEntity<CodeTable>(row));
-
-            //var type = typeof(IEntity);
-            //var types = AppDomain.CurrentDomain.GetAssemblies()
-            //    .SelectMany(s => s.GetTypes())
-            //    .Where(p => type.IsAssignableFrom(p));
-            //foreach (var item in types)
-            //{ 
-            //    var a = CreateMap(typeof(DataRow), item); 
-            //       PropertyInfo[] properties = item.GetProperties();
-            //    foreach (PropertyInfo property in properties)
-            //    {
-            //        a.ForMember(dest => dest.property.name,
-            //        opt => opt.MapFrom(src => src[property.Name])
-            //            );
-            //         if (Table.Columns.Contains(property.Name))
-            //        {
-            //            object value = row[property.Name];
-            //            property.SetValue(entity, value);
-            //        }
-
-            //    }
-
-            //}
-           
+             .ConvertUsing(row => MapDataRowToEntity<CodeTable>(row));          
         }
     }
 }
