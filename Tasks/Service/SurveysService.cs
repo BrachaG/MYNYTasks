367234e8af2e294b8b9aebb6b4ae506f502b74ea
AyalaTamar <214121071@PC3-20>
2023-02-16 17:05:50 +0200

    delete

--- Tasks/Service/SurveysService.cs
+++ Tasks/Service/SurveysService.cs
@@ -13,7 +13,6 @@ namespace Service
             _SqlDataAccess = SqlDataAccess;
             _surveyObjectGenerator = surveyObjectGenerator;
         }
-
         public async Task<List<Survey>> Get(string userId)
         {
             try
