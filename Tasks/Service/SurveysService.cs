8a4606f7158ae3eae0c8c24ff0163a6c57eebd33
yaeligrilak <yaeligrilak@gmail.com>
2023-03-06 16:11:02 +0200

    getSurveyResults

--- Tasks/Service/SurveysService.cs
+++ Tasks/Service/SurveysService.cs
@@ -11,13 +11,17 @@ namespace Service
     {
         ISqlDataAccess _SqlDataAccess;
         IObjectGenerator<Survey> _surveyObjectGenerator;
+        IObjectGenerator<ResultsForSurvey> _resultSurveyObjectGenerator;
+        IObjectGenerator<ResultsForSurveyStudent> _resultSurveyStudentObjectGenerator;
         ILogger<SurveysService> _logger;
 
-        public SurveysService(ISqlDataAccess SqlDataAccess, IObjectGenerator<Survey> surveyObjectGenerator, ILogger<SurveysService> logger)
+        public SurveysService(ISqlDataAccess SqlDataAccess, IObjectGenerator<Survey> surveyObjectGenerator, ILogger<SurveysService> logger, IObjectGenerator<ResultsForSurvey> resultSurveyObjectGenerator, IObjectGenerator<ResultsForSurveyStudent> resultSurveyStudentObjectGenerator)
         {
             _SqlDataAccess = SqlDataAccess;
             _surveyObjectGenerator = surveyObjectGenerator;
             _logger = logger;
+            _resultSurveyObjectGenerator = resultSurveyObjectGenerator;
+            _resultSurveyStudentObjectGenerator = resultSurveyStudentObjectGenerator;
         }
 
         public async Task<List<Survey>> Get()
@@ -37,22 +41,41 @@ namespace Service
             return null;
         }
 
-        public async Task<Survey> Get(int surveyId)
+        public async Task<ResultsForSurvey> Get(int surveyId)
         {
-            _logger.LogDebug("in Get one Survey");
+            _logger.LogDebug("in Get Results For Survey");
             List<SqlParameter> p = new List<SqlParameter>    { new SqlParameter("iSurveyId",surveyId )};
                 
-      /*      try
+            try
             {
-                DataSet dt = await _SqlDataAccess.ExecuteDatasetSP("su_GetResultsForSurvey_SLCT", p);
-                Survey survey = _surveyObjectGenerator.GeneratListFromDataSet(dt);
-                return survey;
+                DataSet ds = await _SqlDataAccess.ExecuteDatasetSP("su_GetResultsForSurvey_SLCT", p);
+                DataTable dtStudent = ds.Tables[0];
+                DataTable dtQuestions = ds.Tables[1];
+                DataTable dtAnswers = ds.Tables[2];
+                List<ResultsForSurveyStudent> students = _resultSurveyStudentObjectGenerator.GeneratListFromDataTable(dtStudent);
+                List<string> questions=new List<string>();
+                foreach (DataRow question in dtQuestions.Rows)
+                {
+                    questions.Add(question[0].ToString());
+                }
+                foreach (DataRow answer in dtAnswers.Rows)
+                {
+                    ResultsForSurveyStudent student =students.Find(s => s.iStudentId == int.Parse(answer[0].ToString()));
+                    if (student != null)
+                    {
+                        student.lAnswers.Add(answer[2].ToString());
+                    }
+                }
+                ResultsForSurvey results = new ResultsForSurvey();
+                results.lResultsForSurveyStudent = students;
+                results.lQuestions = questions;
+                return results;
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex, "in SurveyService, get all survey, faild when trying to approach to database");
+                _logger.LogError(ex, "in SurveyService, get results for survey, faild when trying to approach to database");
                 var b = ex.Message;
-            }*/
+            }
             return null;
         }
     }
--- Tasks/Tasks/Controllers/SurveysController.cs
+++ Tasks/Tasks/Controllers/SurveysController.cs
@@ -7,17 +7,15 @@ using Service;
 namespace Tasks.Controllers
 {
     [Route("api/[controller]")]
-    [ApiController, Authorize]
+    [ApiController]//, Authorize
     public class SurveysController : ControllerBase
     {
         ISurveysService _SurveysService;
         ILogger<SurveysController> _logger;
         public SurveysController(ISurveysService SurveysService, ILogger<SurveysController> logger)
         {
-
             _SurveysService = SurveysService;
             _logger = logger;
-
         }
         [HttpGet("Get")]
         public async Task<List<Survey>> Get()
@@ -26,8 +24,13 @@ namespace Tasks.Controllers
             string Type = JwtRegisteredClaimNames.Sub;
             string userId = User.Claims.FirstOrDefault(c => c.Type == Type).Value;
             return await _SurveysService.Get();
-
-            ;
+        }
+        [HttpGet("Get/{id}")]
+        public async Task<ResultsForSurvey> GetResultsForSurvey(int id)
+        {
+            _logger.LogDebug("Get ResultsForSurvey");
+           
+            return await _SurveysService.Get(id);
         }
     }
 }
