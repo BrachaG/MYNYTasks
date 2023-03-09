35427aa68168b0f927ba6a2ece52d896a5495696
Tovi <tovi7751@gmail.com>
2023-03-09 14:51:33 +0200

    multiple

--- ny-tasks/src/app/pages/survey-results/survey-results.component.ts
+++ ny-tasks/src/app/pages/survey-results/survey-results.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
 import {AccordionModule} from 'primeng/accordion';
+
 @Component({
   selector: 'app-survey-results',
   templateUrl: './survey-results.component.html',
-  styleUrls: ['./survey-results.component.css']
+  styleUrls: ['./survey-results.component.scss']
 })
 export class SurveyResultsComponent {
 
