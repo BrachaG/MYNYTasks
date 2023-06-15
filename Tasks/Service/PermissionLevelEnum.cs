0098f326934f0d93909de046df87fc6620c51e21
ayalaTamar <tamar37860@gmail.com>
2023-06-15 12:51:04 +0300

    fix

--- Tasks/Service/PermissionLevelEnum.cs
+++ Tasks/Service/PermissionLevelEnum.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Service
+﻿namespace Service
 {
     public class PermissionLevelEnum
     {
         public enum PermissionLevel { NYmanagar = 1, coordinator = 2, SystemManager = 3 };
     }
-    }
+}
 
