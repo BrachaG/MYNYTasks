2f610b864cfed25fee27dede81e1f5d561881185
AyalaTamar <214121071@PC3-20>
2023-06-18 12:56:21 +0300

    in middle design need responsive

--- Tasks/Repository/SqlDataAccess.cs
+++ Tasks/Repository/SqlDataAccess.cs
@@ -14,7 +14,7 @@ public class SqlDataAccess : ISqlDataAccess
     {
         _logger = logger;
         _Configuration = Configuration;
-        connectionString = _Configuration.GetConnectionString("Home");
+        connectionString = _Configuration.GetConnectionString("DefaultConnection");
     }
     #region ExecuteDatasetSP
     public async Task<DataSet> ExecuteDatasetSP(string spName, List<SqlParameter> SPParameters)
