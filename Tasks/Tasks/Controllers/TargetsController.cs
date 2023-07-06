ee418c4d6ae8d52884188154a886a7f99af1ea26
ayalatamar <36214121071@mby.co.il>
2023-07-06 11:19:13 +0300

    mooving spaces

--- Tasks/Tasks/Controllers/TargetsController.cs
+++ Tasks/Tasks/Controllers/TargetsController.cs
@@ -16,9 +16,6 @@ namespace Tasks.Controllers
         {
             _logger = logger;
             _targetsService = targetsService;
-           
-          
-          
         }
         [HttpGet("Get")]
         public async Task<ActionResult<List<Target>>> GetTargetsByUserId()
