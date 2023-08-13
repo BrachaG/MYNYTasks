b0d968f8dc8f3eae1966f5c655b92078bab069be
EsterTaitelbaum <ester52995@gmail.com>
2023-08-13 13:48:25 +0300

    change SurveysService

--- Tasks/Service/SurveysService.cs
+++ Tasks/Service/SurveysService.cs
@@ -31,58 +31,9 @@ namespace Service
             catch (Exception ex)
             {
                 _logger.LogError(ex, "in SurveyService, get all survey, faild when trying to approach to database");
-               return new ObjectResult(null) { StatusCode = 500 };
-            }
-
-        public async Task<ActionResult<ResultsForSurvey>> Get(int surveyId, int permissionId, int? branchId = null)
-        {
-            _logger.LogDebug("in Get Results For Survey");
-            List<SqlParameter> p = new List<SqlParameter> {
-                new SqlParameter("iSurveyId", surveyId) ,
-                new SqlParameter("iPermissionLevelId", permissionId),
-                new SqlParameter("iBranchId", branchId)
-            };
-            try
-            {
-                DataSet ds = await _sqlDataAccess.ExecuteDatasetSP("su_GetResultsForSurvey_SLCT", p);
-                if (ds.Tables[0] != null && ds.Tables[1] != null && ds.Tables[2] != null && ds.Tables[3] != null)
-                {
-                    DataTable dtStudent = ds.Tables[0];
-                    DataTable dtQuestions = ds.Tables[1];
-                    DataTable dtAnswers = ds.Tables[2];
-                    DataTable dtOptions = ds.Tables[3];
-
-                    List<ResultsForSurveyStudent> students = _resultSurveyStudentObjectGenerator.GeneratListFromDataTable(dtStudent);
-                    List<Question> questions = _questionObjectGenerator.GeneratListFromDataTable(dtQuestions);
-                    List<Answer> answers = _answerObjectGenerator.GeneratListFromDataTable(dtAnswers);
-                    List<Options> options = _optionsObjectGenerator.GeneratListFromDataTable(dtOptions);
-                    foreach (Answer answer in answers)
-                    {
-                        ResultsForSurveyStudent student = students.Find(s => s.iStudentId == answer.iStudentId);
-                        if (student != null)
-                        {
-                            student.lAnswers.Add(answer);
-                            student.image = _configuration["ImageUrl"] + student.iStudentId + ".png";
-                        }
-                    }
-                    ResultsForSurvey results = new ResultsForSurvey();
-                    results.lResultsForSurveyStudent = students;
-                    results.lQuestions = questions;
-                    results.lOptions = options;
-                    return results;
-                }
-                return null;
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "in SurveyService, get results for survey, faild when trying to approach to database");
-                return new StatusCodeResult(400);
+                return new ObjectResult(null) { StatusCode = 500 };
             }
 
         }
-
-
-
     }
-
 }
