6cea7df855ae5e37d533f24ac93fa46b23235001
ayalatamar <36214121071@mby.co.il>
2023-08-28 18:16:36 +0300

    in middle design

--- ny-tasks/src/app/pages/tasks/tasks.component.ts
+++ ny-tasks/src/app/pages/tasks/tasks.component.ts
@@ -9,10 +9,10 @@ import { Task } from 'src/models/Task.model';
   styleUrls: ['./tasks.component.scss']
 })
 export class TasksComponent {
-  tasks: Task[]=[];
+  tasks: Task[] = [];
   sum: number = 0;
   filterText: string = '';
-  filteredTasks: any;
+  filteredTasks: Task[] = [];
   filterTimeout: any;
   filterVisible: boolean = true;
 
@@ -25,25 +25,29 @@ export class TasksComponent {
 
   applyFilter() {
     clearTimeout(this.filterTimeout); // Clear any existing timeout
-    // this.filterTimeout = setTimeout(() => {
-    //   console.log(this.filterText);
+    this.filterTimeout = setTimeout(() => {
+      console.log(this.filterText);
 
-    //   const filterValue = this.filterText.toLowerCase();
-    //   this.filteredTasks = this.surveys.filter((survey) => {
-    //     // Apply your desired filtering logic based on the survey properties
-    //     return (
-    //       survey.nvSurveyName.toLowerCase().includes(filterValue) ||
-    //       survey.dtEndSurveyDate.toString().includes(filterValue) ||
-    //       survey.iRespondentsCount.toString().includes(filterValue) ||
-    //       survey.iQuestionCount.toString().includes(filterValue) 
-    //   );
-    //   // });
-    // }, 1000);
+      const filterValue = this.filterText.toLowerCase();
+      this.filteredTasks = this.tasks.filter((task) => {
+        // Apply your desired filtering logic based on the survey properties
+        return (
+          task.iType.toString().includes(filterValue) ||
+          task.nvCategory.toLowerCase().includes(filterValue) ||
+          task.iStudentId.toString().includes(filterValue) ||
+          task.iStatus.toString().includes(filterValue) ||
+          task.iTargetId.toString().includes(filterValue) ||
+          task.nvOrigin.toLowerCase().includes(filterValue) ||
+          task.dtEndDate.toString().includes(filterValue)
+        );
+      });
+    }, 1000);
   }
   getAllTasks() {
     this.taskService.getTasks().subscribe((res: any) => {
       this.tasks = res;
-      console.log(this.tasks);   
+      this.filteredTasks = res;
+      console.log(this.tasks);
       this.sum = this.tasks.length;
     })
     console.log(this.tasks);
