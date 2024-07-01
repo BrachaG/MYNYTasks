9ca2670619fbe961cbb6003499d6ac30a7285929
OdayaBareket <94451527+OdayaBareket@users.noreply.github.com>
2024-07-01 12:27:09 +0300

    any changes in names of variables

--- Tasks/Tasks/sqlScripts/createTargetsCodeTable.sql
+++ Tasks/Tasks/sqlScripts/createTargetsCodeTable.sql
@@ -1,7 +1,7 @@
 USE[NefeshYehudi]
 CREATE TABLE tbl_code_Targets (
   iTypeTargetId INT PRIMARY KEY IDENTITY(1,1),
-  nvTargetName NVARCHAR(50),
+  nvTypeTargetName NVARCHAR(50),
   iLastUpdateUserId INT,
   dtLastUpdateDate DATETIME,
   istsRowStatus BIT
